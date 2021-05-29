71ce9755a15672d87870fe6ec98569c1f98be3bc
Giorgi Nikolaishvili <gnikolai@uoregon.edu>
2021-05-29 03:44:52 -0700

    Minor fixes -- InitializeParameters and InitializeWageValueDecision are functional

--- bils_chang_kim_2011/steady_state_equilibrium.jl
+++ bils_chang_kim_2011/steady_state_equilibrium.jl
@@ -14,9 +14,9 @@ using LaTeXStrings
 # Initialize parameters & asset grids
 
 """
-    InitializeParameters()  
+    InitializeParameters  
 """
-mutable struct InitializeParameters() 
+@with_kw mutable struct InitializeParameters
     prod::Float64 = 1.0 # output
     irate::Float64 = 0.06 # annual interest rate 
     α::Float64 = 0.5 # worker's share of surplus 
@@ -45,28 +45,42 @@ end
     InitializeWageValueDecision()
 """
 function InitializeWageValueDecision(para::InitializeParameters)
-    @unpack prod, β, λ, UI, B_leisure, match_prob, agrid, β_monthly = para 
+
+    @unpack prod, β, λ, UI, B_leisure, match_prob, agrid, irate_monthly, β_monthly = para 
+
+    # Initialize value functions and Nash-bargaining wages 
+    # compute Nash-bargaining wage for a assuming that a is fixed 
     βpth = 1.0/(1-β*(1-λ)+β*match_prob) 
+
     N = length(agrid) 
     AE = zeros(N)
     AU = zeros(N) 
     Wage = zeros(N)
-    Util_W = zeros(N)
-    Util_U = zeros(N)
+    Util_W = 0.0
+    Util_U = 0.0
     W = zeros(N)
     U = zeros(N)
-    J = zeros(N) 
+    J = zeros(N)
+    
+    # Fill out empty vectors 
     for i in 1:N 
-        AE[i] = agrid(i) 
-        AU[i] = agrid(i)
+        # Initialized asset decision rules 
+        AE[i] = agrid[i] 
+        AU[i] = agrid[i]
+        # Solve Nash-bargaining wages given AE and AU
         Wage[i] = prod 
+        # Initialize value functions given AE, AU, and Wage
         Util_W = log(irate_monthly*agrid[i]+Wage[i])
         Util_U = log(irate_monthly*agrid[i]+UI) + B_leisure 
         W[i] = (Util_W - β*λ*βpth*(Util_W-Util_U))/(1-β)
         U[i] = (Util_U - β*match_prob*βpth*(Util_W-Util_U))/(1-β)
         J[i] = (prod-Wage[i])/(1.0-β_monthly*(1.0-λ))
     end 
+
+    return (AE, AU, Wage, Util_W, Util_U, W, U, J) 
 end 
+
+#AE, AU, Wage, Util_W, Util_U, W, U, J =  InitializeWageValueDecision(InitializeParameters())
 
 # ###################################
 # ###################################
