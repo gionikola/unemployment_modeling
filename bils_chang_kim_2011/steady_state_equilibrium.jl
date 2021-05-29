f74ac12e61803c84597b118c3a42682ed81219c3
Giorgi Nikolaishvili <gnikolai@uoregon.edu>
2021-05-29 01:16:33 -0700

    Add steady_state_equilibrium script outline

--- bils_chang_kim_2011/steady_state_equilibrium.jl
+++ bils_chang_kim_2011/steady_state_equilibrium.jl
@@ -1,3 +1,14 @@
-test = 2
-test2 = test + 1
-println(test2)
+
+# Initialize parameters & asset grids
+
+# Initialize value functions and Nash-bargaining wages
+
+# Find equilibriun Nash-bargaining wages 
+
+# Compute time-invariant measures
+
+# Compute new θ using free entry condition
+
+# Compute aggregate variables 
+
+# Generate results 
