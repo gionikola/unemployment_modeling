656cca4eacebd47defacc08aa45f65e51579a9d6
Giorgi Nikolaishvili <gnikolai@uoregon.edu>
2021-05-29 01:40:43 -0700

    Add function placeholders to steady_state_equilibrium

--- bils_chang_kim_2011/steady_state_equilibrium.jl
+++ bils_chang_kim_2011/steady_state_equilibrium.jl
@@ -1,14 +1,107 @@
-
+# ###################################
+# ###################################
 # Initialize parameters & asset grids
 
+"""
+    InitializeParameters() 
+"""
+function InitializeParameters() 
+
+end 
+
+"""
+    InitializeGrid()
+"""
+function InitializeGrid() 
+
+end 
+
+# ###################################
+# ###################################
 # Initialize value functions and Nash-bargaining wages
 
+"""
+    InitializeWageValueDecision()
+"""
+function InitializeWageValueDecision()
+
+end 
+
+# ###################################
+# ###################################
 # Find equilibriun Nash-bargaining wages 
 
+"""
+    EquilibriumWage()
+""" 
+function EquilibriumWage()
+
+end 
+
+# ###################################
+# ###################################
 # Compute time-invariant measures
 
+"""
+    InitializeDAgrid()
+"""
+function InitializeDAgrid()
+
+end 
+
+"""
+    InitializeMeasures()
+"""
+function InitializeMeasures()
+
+end 
+
+"""
+    InverseAssetDecisions()
+"""
+function InverseAssetDecision()
+
+end 
+
+"""
+    InvariantMeasures()
+"""
+function InvariantMeasures()
+
+end 
+
+# ###################################
+# ###################################
 # Compute new θ using free entry condition
 
+# Insert code 
+
+# ###################################
+# ###################################
 # Compute aggregate variables 
 
+"""
+    AggregateVariables()
+"""
+function AggregateVariables()
+
+end 
+
+# ###################################
+# ###################################
 # Generate results 
+
+"""
+Steps:
+(1) Initialize parmameters and asset grids
+(2) Initialize value functions and Nash-bargaining wages 
+(3) Find equilibrun Nash-bargaining wages
+(4) Compute time-invariant measures 
+(5) Compute new θ
+(6) Compute aggregate variables
+"""
+
+
+
+# ###################################
+# ###################################
