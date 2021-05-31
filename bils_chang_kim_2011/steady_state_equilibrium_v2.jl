dbb8fd85314d691c4116825100ece1c15456cc55
Giorgi Nikolaishvili <gnikolai@uoregon.edu>
2021-05-31 00:10:40 -0700

    Fix parameters + Add Tauchen estimator

--- bils_chang_kim_2011/steady_state_equilibrium_v2.jl
+++ bils_chang_kim_2011/steady_state_equilibrium_v2.jl
@@ -1,14 +1,11 @@
 using Parameters 
 using LinearAlgebra 
 using Statistics
-using BasisMatrices
 using Distributions
 using Optim
-using BasisMatrices 
 using SparseArrays 
-using Plots 
-using LaTeXStrings 
-#using FastGaussQuadrature
+using QuantEcon
+using SpecialFunctions
 
 # ######################################################################
 # ######################################################################
@@ -17,30 +14,32 @@ using LaTeXStrings
 # Initialize parameters & asset grids
 
 """
-    ModelParameters
+    ModelParams
 """
-@with_kw mutable struct ModelParameters
+@with_kw mutable struct ModelParams
     A::Float64              = 1.0                           # output productivity factor
     r::Float64              = 0.06                          # annual interest rate 
     r_mon::Float64          = (1.0 + r)^(1.0/12.0) - 1.0    # monthly interest rate 
-    α::Float64              = 0.5                           # worker's share of surplus 
+    worker_surplus::Float64 = 0.5                           # worker's share of surplus 
     β::Float64              = 0.995133                      # discount factor 
     β_mon::Float64          = 1.0/(1.0 + r_mon)             # monthly depreciation rate 
     γ::Float64              = 1.0                           # curvature of utility function 
     λ::Float64              = 0.02                          # exogenous separation rate 
-    UI::Float64             = 0.4                           # UI benefits 
+    b::Float64              = 0.4                           # UI benefits 
     B::Float64              = 0.0                           # utility from leisure 
     κ::Float64              = 0.522                         # vacancy posting cost with log utility 
     θ::Float64              = 1.0                           # labor market tightness, v/u, normalized to 1
-    match_pow::Float64      = 0.5                           # power term in the matching function 
-    match_scale::Float64    = 0.3133                        # scale parameter in the matching function 
-    match_prob::Float64     = match_scale*θ^(match_pow)     # matching probability 
+    α::Float64              = 0.5                           # power term in the matching function 
+    η::Float64              = 0.3133                        # scale parameter in the matching function
+    p_θ::Float64            = η*θ^α                         # unemployed worker matching rate  
+    q_θ::Float64            = η*θ^(α-1)                     # vacancy matching probability 
     amin::Float64           = -6.0                          # minimum asset 
     amax::Float64           = 120.0                         # maximum asset 
     agrid::Vector{Float64}  = LinRange(amin, amax, 100)     # asset grid 
-    xmin::Float64           = 0.5                           # minimum match productivity 
-    xmax::Float64           = 1.5                           # maximum match productivity 
-    xgrid::Vector{Float64}  = LinRange(xmin, xmax, 5)       # match productivity grid 
+    ρ_x::Float64            = 0.97                          # persistence of idiosyncratic productivity ln(x)
+    σ_x::Float64            = 0.13                          # std. dev. of innovation to ln(x) 
+    π_x                     = TauchenApprox(9, ρ_x, σ_x).distribution           # probability weights on ln(x) 
+    xgrid                   = exp.(TauchenApprox(9, ρ_x, σ_x).state_values)     # match productivity grid 
 end 
 
 # ######################################################################
@@ -51,7 +50,7 @@ end
 """
 Household Bernoulli utility function
 """
-function utility(para::ModelParameters, c::Real, l::Real)
+function utility(para::ModelParams, c::Real, l::Real)
     
     γ = para.γ # Store risk-aversion parameter
     B = para.B # Store the leisure utility coefficient
@@ -62,7 +61,7 @@ function utility(para::ModelParameters, c::Real, l::Real)
         if γ == 1
           return log(c) + B*l # if σ = 1, CRRA utility is log
         else
-          return (c^(1-γ))/(1-γ) + B*l # if σ != 1, regular specification
+          return (c^(1-γ)-1)/(1-γ) + B*l # if σ != 1, regular specification
         end
     else
         return -Inf # infinite disutility if c∈(-∞,0]
@@ -75,35 +74,82 @@ end;
 # ######################################################################
 # 
 """
-  iterateBellman(agent::Agent,Vcont)
-
-Iterates on the bellman equation using continuation value function Vcont
+    TauchenApprox
 """
-function iterateBellman(para::ModelParameters, W_old, U_old, J_old, V_old)
+function TauchenApprox(N::Integer, ρ::T1, σ::T2, μ=zero(promote_type(T1, T2)), n_std::Integer=3) where {T1 <: Real, T2 <: Real}
+    
+    std_norm_cdf(x::T) where {T <: Real} = 0.5 * SpecialFunctions.erfc(-x/sqrt(2))
+    std_norm_cdf(x::Array{T}) where {T <: Real} = 0.5 .* SpecialFunctions.erfc(-x./sqrt(2))
 
-    @unpack γ = para #unpack parameters
+    # Get discretized space
+    a_bar = n_std * sqrt(σ^2 / (1 - ρ^2))
+    y = range(-a_bar, stop=a_bar, length=N)
+    d = y[2] - y[1]
+
+    # Get transition probabilities
+    Π = zeros(promote_type(T1, T2), N, N)
+    for row = 1:N
+        # Do end points first
+        Π[row, 1] = std_norm_cdf((y[1] - ρ*y[row] + d/2) / σ)
+        Π[row, N] = 1 - std_norm_cdf((y[N] - ρ*y[row] - d/2) / σ)
+
+        # fill in the middle columns
+        for col = 2:N-1
+            Π[row, col] = (std_norm_cdf((y[col] - ρ*y[row] + d/2) / σ) -
+                           std_norm_cdf((y[col] - ρ*y[row] - d/2) / σ))
+        end
+    end
+
+    # state values 
+    ȳ = y .+ μ / (1 - ρ) # center process around its mean (wbar / (1 - rho)) in new variable
+    
+    # renormalize transition matrix 
+    Π = Π./sum(Π, dims = 2)
+
+    # Generate stationary distribution from Π
+    N = size(Π)[1]
+    π0 = ones(1,N)/N
+    diff = 1.
+    while diff > 1e-10
+        π1 = π0*Π
+        diff = norm(π1-π0,Inf)
+        π0 = π1
+    end
+    π0 = copy(π0') 
+
+    return (transition = Π, distribution = π0, state_values = ȳ)
+end
+
+# ######################################################################
+# ######################################################################
+# ######################################################################
+# ######################################################################
+# 
+"""
+  iterateBellman()
+
+Iterates on the bellman equation using continuation value function.
+"""
+function iterateBellman(para::ModelParams, W_old, U_old, J_old, V_old)
+
+    @unpack agrid, xgrid, π_x = para #unpack parameters
 
     u(c,l) = utility(para,c,l) #shorthand for utility
 
-end;
+    N_x = length(xgrid)
+    N_a = length(agrid)
 
-function constructTransitionMatrix(agent::Agent,a_policy)
-    @unpack π,agrid = agent
+    #solve for each state
+    #preallocate space for V and a_policy
 
-    N = length(agrid)
-    S = length(π)
+    for x_i  in 1:N_x
+        for a_i_ in 1:N_a
+            for a′_i in 1:N_a 
+                
+            end 
 
-    H = spzeros(N*S,N*S) #sparse matrix to save on space
-    for n_ in 1:N
-        for s in 1:S
-            i_ = n_+N*(s-1) # index for ia_,s
-            n = a_policy[n_,s]
-            for sprime in 1:S
-                i = n+N*(sprime-1)
-                #probability of transitioning from state ia_,s to ia,sprime
-                H[i_,i] = π[sprime]
-            end
         end
     end
-    return H
+
+    return W_new, U_new, J_new, V_new 
 end;
